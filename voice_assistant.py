3be1e1f6a9fdc754ff666de9328e0ad3c483af81
Lea Foy <leafoy@gmail.com>
2022-11-02 21:39:09 -0400

    Add transcription option

--- voice_assistant.py
+++ voice_assistant.py
@@ -10,8 +10,7 @@ import wolframalpha
 # https://developer.wolframalpha.com/portal/myapps/index.html
 import pyttsx3
 # https://pyttsx3.readthedocs.io/en/latest/engine.html
-import smtplib
-# https://docs.python.org/3/library/smtplib.html
+from pathlib import Path
 
 # Set engine to Pyttsx3 for text-to-speech. Sapi5 is a Microsoft speech application platform interface.
 engine = pyttsx3.init('sapi5')
@@ -27,7 +26,7 @@ def speak(audio):
     engine.runAndWait()
 
 # Greet user
-def wish_me():
+def greeting():
 
     hour = int(datetime.datetime.now().hour)
     helper_name = ("Gertrude the Terrible")
@@ -59,6 +58,8 @@ def get_voice_input():
     recognizer = sr.Recognizer()
 
     with sr.Microphone() as source:
+        # Listens for 1 second to calibrate for ambient noise
+        recognizer.adjust_for_ambient_noise(source)
         print("Listening...")
         # Represents minimum length of silence will register as end of a phrase
         recognizer.pause_threshold = 1
@@ -84,7 +85,7 @@ if __name__ == "__main__":
     clear = lambda: os.system("cls")
     clear()
     # Greet user and get user's name
-    wish_me()
+    greeting()
     user_name = get_name()
     # Create Wolfram Alpha client with api key
     wolf_api_id = "P3VJ6A-97RR5EGKVT"
@@ -95,7 +96,55 @@ if __name__ == "__main__":
         query = get_voice_input().lower()
         search_query = query.replace(" ", "+")
 
-        if "wikipedia" in query:
+        # Create new file and add text to it
+        if "new file" in query:
+            speak("What should I name your file?")
+            file_name = get_voice_input().replace(" ", "_")
+
+            with open(f"{file_name}.txt", "w") as text:
+                speak("What would you like to write?")
+                transcript = get_voice_input()
+                text.write(transcript)
+                speak(f"I have added the following: {query}")
+
+        # Open new file and add text to it
+        elif "open file" or "transcribe" in query:
+            query = ""
+
+            speak("Here are the files you can open")
+            # Get filepath and save any text files in a list
+            path = Path(__file__).parent.absolute()
+            files = [x for x in os.listdir(path) if x.endswith(".txt")]
+            
+            # If there are text files in the folder, get name of folder to open and open it
+            if files:
+                print(files)
+                speak(files)
+                speak("Which file do you want opened?")
+                file_name = get_voice_input()
+
+                for file in files:
+                    if file_name in file:
+                        with open(file, "a+") as document:
+                            speak(f"I've opened {file}. Would you like me to read its contents?")
+                            answer = get_voice_input()
+                            if "yes" in answer:
+                                existing_text = document.read()
+                                print(existing_text)
+                                speak(existing_text)
+
+                            speak(f"Would you like to add to this file?")
+
+                            answer = get_voice_input()
+                            if "yes" in answer:
+                                speak(f"OK I'm listening.")
+                                transcription = get_voice_input()
+                                document.write(transcription)
+                                speak(f"Your text has been added.")
+            else:
+                speak("There are currently no text files in this folder.")
+
+        elif "wikipedia" in query:
             speak("Searching Wikipedia...")
             query = query.replace("wikipedia", "")
             # Returns first 4 sentences of result
@@ -137,7 +186,8 @@ if __name__ == "__main__":
             speak(f"{query} is {answer}")
             
         # Use the Wolfram Alpha API to answer general queries
-        elif "what is" in query or "who is" in query or "what are" in query:
+        # elif "what is" in query or "who is" in query or "what are" in query:
+        elif len([x for x in ["what is", "who is", "what are" ] if x in query]) > 0:
             response = client.query(query)
             answer = next(response.results).text
             print(answer)
@@ -150,7 +200,10 @@ if __name__ == "__main__":
         elif "don't listen" in query or "stop listening" in query:
             speak(f"How many seconds should I stop listening for?")
             sleep_time = int(get_voice_input())
-            time.sleep(sleep_time)
+            if sleep_time:
+                speak(f"Very well.")
+                time.sleep(sleep_time)
+            speak(f"Hello my friend, I am back!")
 
         elif "exit" in query:
             speak(f"Goodbye {user_name}, may the force be with you")
